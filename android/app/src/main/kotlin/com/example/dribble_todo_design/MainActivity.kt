f231fbd227124c69499190c56855e030d828f651
Gabriel Victor <dester.gvpdv@gmail.com>
2019-11-11 17:28:16 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/dribble_todo_design/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dribble_todo_design
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
